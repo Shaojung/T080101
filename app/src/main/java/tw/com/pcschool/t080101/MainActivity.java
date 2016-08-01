1c3ac4faaa047c1e75d37ba9bf5851ceca5c4a1f
shaojung <shaojung@gmail.com>
2016-08-01 15:07:23 +0800

    Read String

--- app/src/main/java/tw/com/pcschool/t080101/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080101/MainActivity.java
@@ -2,6 +2,17 @@ package tw.com.pcschool.t080101;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +23,26 @@ public class MainActivity extends AppCompatActivity {
 
         new Thread(){
             public void run(){
+                String strurl = "http://udn.com/rssfeed/news/1";
+                try {
+                    URL url = new URL(strurl);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream();
+                    BufferedReader r = new BufferedReader(new InputStreamReader(is));
+                    String x = "";
+                    do {
+                        Log.d("READ", x);
+                        x = r.readLine();
+                    }while (!x.equals(""));
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
         }.start();
